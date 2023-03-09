5912035ff5ecea3770b9747cfa6ede43041bb1e8
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-03-09 12:16:35 +0700

    import router on for beranda & /api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './layouts/Header'
+import BerandaContent from "./BerandaContent";
+import Footer from './layouts/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={
+          <>
+            <Header/>
+            <BerandaContent/>
+            <Footer/>
+          </>
+        } />
+        <Route path='/api' element={
+          <>
+            <Header/>
+            <BerandaContent/>
+            <Footer/>
+          </>
+        } />
+      </Routes>
+    </>
   );
 }
 
